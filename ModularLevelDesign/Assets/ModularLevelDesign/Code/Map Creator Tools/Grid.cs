2677f5936797861623ea9ba927b93bb8b3517180
BlakesDV <dmunoz@cocoschool.mx>
2025-04-19 23:42:31 -0600

    Grid structure bases

--- ModularLevelDesign/Assets/ModularLevelDesign/Code/Map Creator Tools/Grid.cs
+++ ModularLevelDesign/Assets/ModularLevelDesign/Code/Map Creator Tools/Grid.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static Grid Instance;
+    public int width = 10, height = 10;
+    public float cellsize = 1f;
+    public GameObject modulePrefab;
+    private GameObject[,] grid;
+
+    private void Awake()
     {
-        
+        Instance = this;
+        grid = new GameObject[width, height];
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector2Int GridWorldPos(Vector3 worldPos)
     {
-        
+        int x = Mathf.RoundToInt(worldPos.x / cellsize);
+        int y = Mathf.RoundToInt(worldPos.y / cellsize);
+        return new Vector2Int(x, y);
     }
+
+    public void PlaceModule(Vector3 worldPos)
+    {
+        Vector2Int gridPos = GridWorldPos(worldPos);
+        //If for placing the module inside the grid position
+        //if ()
+        //{
+
+        //}
+    }
+    //Create function for checking module sides
+    //Create function for checking if the module is inside the grid
 }
