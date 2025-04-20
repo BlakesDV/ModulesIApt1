135af340df2ba02d21141d639f176857f6b3e200
BlakesDV <dmunoz@cocoschool.mx>
2025-04-19 21:10:14 -0600

    Grid script imp

--- ModularLevelDesign/Assets/ModularLevelDesign/Code/Module.cs
+++ ModularLevelDesign/Assets/ModularLevelDesign/Code/Module.cs
@@ -4,6 +4,17 @@ namespace ProceduralLevelDesign
 {
     public class Module : MonoBehaviour
     {
-        
+        private Vector2Int gridPos;
+
+        public void SetPosition(Vector2Int pos)
+        {
+            gridPos = pos;
+
+        }
+
+        public void UpdateVisual()
+        {
+            //bool left = Grid.Instantiate.has
+        }
     }
 }
